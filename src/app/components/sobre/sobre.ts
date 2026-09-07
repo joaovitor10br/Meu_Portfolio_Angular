60e05e8f0f5e51b7691ed3350bb7fabbb63c0070
João Vitor <joaovitoralvesmartins10@gmail.com>
2026-09-07 12:46:47 -0300

    Alteração no texto da página sobre

--- src/app/components/sobre/sobre.ts
+++ src/app/components/sobre/sobre.ts
@@ -10,5 +10,5 @@ export class Sobre {
   nome = 'João Vitor';
   fotoUrl = 'foto-perfil.jpeg';
   curriculoUrl = 'meu_curriculo_joao_vitor.pdf';
-  descricao = 'Estudante de Engenharia da Computação, programando desde 2021. Tenho interesse tanto em back-end quanto em front-end, e nas horas vagas gosto de desenvolver jogos usando Unity e Godot.';
+  descricao = 'Desenvolvedor com foco em back-end e front-end, atuando com Python, Java, JavaScript, TypeScript, C e C#. Formação em Engenharia da Computação e projetos práticos como sistemas de backup em Linux. Busco sempre entregar soluções sob medida, com atenção a detalhes e prazos.';
 }
