9cbc595fc87b76321c87b636321c7e7ffe626876
João Vitor <joaovitoralvesmartins10@gmail.com>
2026-09-07 19:12:00 -0300

    Correção da descrição do texto sobre

--- src/app/components/sobre/sobre.ts
+++ src/app/components/sobre/sobre.ts
@@ -10,5 +10,5 @@ export class Sobre {
   nome = 'João Vitor';
   fotoUrl = 'foto-perfil.jpeg';
   curriculoUrl = 'meu_curriculo_joao_vitor.pdf';
-  descricao = 'Desenvolvedor com foco em back-end e front-end, atuando com Python, Java, JavaScript, TypeScript, C e C#. Além de projetos práticos como sistemas de backup em Linux. Busco sempre entregar soluções sob medida, com atenção a detalhes e prazos.';
+  descricao = 'Desenvolvedor com foco em back-end e front-end, atuando com Python, Java, JavaScript, TypeScript, C e C#. Além de projetos práticos como sistemas de backup em Linux, busco sempre entregar soluções sob medida, com atenção a detalhes e prazos.';
 }
